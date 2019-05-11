e14b17e5fdf0c8e17b1ce805d26b9658c61c1765
Maxim <Maxim.Zamiatin@endava.com>
2019-05-11 23:26:06 +0300

    Second update to .doesFilterWorkOK() method (Test 2)

--- src/main/java/com/endava/booking/atf/page/GetawayDealsSearchResultsPage.java
+++ src/main/java/com/endava/booking/atf/page/GetawayDealsSearchResultsPage.java
@@ -1,12 +1,12 @@
 package com.endava.booking.atf.page;
 
 import com.endava.booking.atf.common.action.BasePage;
-import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -52,8 +52,15 @@ public class GetawayDealsSearchResultsPage extends BasePage {
 
     public void doesFilterWorkOK() throws InterruptedException {
         WebDriverWait wait = new WebDriverWait(driver, 60);
+//        wait.until(new ExpectedCondition<Boolean>() {
+//            public Boolean apply(WebDriver driver) {
+//                return (jse).executeScript(
+//                        "return document.readyState"
+//                ).equals("complete");
+//            }
+//        });
+
         //wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.xpath("//sup[@class='sr_room_reinforcement'][contains(text(),'Breakfast')]")));
-        //wait.until(driver -> (jse).executeScript("return document.readyState").equals("complete"));
         Thread.sleep(5000);
         if (breakfastReinforcementMessage.size() == 20) System.out.println("Filter works OK");
         else System.out.println("Getaway deals list is not filtered properly");
