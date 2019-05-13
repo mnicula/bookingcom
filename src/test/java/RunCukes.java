295861037f00c1560e5bcdb9ee7ed99133614c94
Alexei Serbu <alexeiserbu@gmail.com>
2019-05-13 11:02:08 +0300

    Merge branch 'master' of C:\Users\aserbu\bookingcom with conflicts.

--- src/test/java/RunCukes.java
+++ src/test/java/RunCukes.java
@@ -6,7 +6,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
 
-        tags = {""},//scenarios to run
+        tags = {"@1"},//scenarios to run
         glue = {"hooks" , "stepDefinition"},
         plugin = { "html:target/cucumber-reports",
                 "pretty",
